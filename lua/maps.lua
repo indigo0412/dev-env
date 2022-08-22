c4757d6c96af1612761154e7df28fb7475b1ff83
GyeongJin Lee <gyeongjinlee@indigo0412-l2.local>
2022-08-23 00:18:43 +0900

    keymap set for resize window

--- lua/maps.lua
+++ lua/maps.lua
@@ -33,3 +33,8 @@ keymap.set('', 'sj', '<C-w>j')
 keymap.set('', 'sl', '<C-w>l')
 
 -- Resize window
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
+

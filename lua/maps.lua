0e0be7201a064b23f2c438c9e2c870fb192996d1
GyeongJin Lee <gyeongjinlee@indigo0412-l2.local>
2022-08-22 21:03:36 +0900

    init neovim env setting

---
+++ lua/maps.lua
@@ -0,0 +1,23 @@
+local keymap = vim.keymap
+
+-- do not yank with x
+keymap.set('n', 'x', '"_x')
+
+
+-- Increment/Decremant 
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+
+-- New tab    tab move key   gt
+keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+

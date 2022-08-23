960513166ca2c615c68262a409e0c177e6c99811
GyeongJin Lee <gyeongjinlee@indigo0412-l2.local>
2022-08-23 10:49:51 +0900

    setting clipboard macos

--- init.lua
+++ init.lua
@@ -2,3 +2,21 @@ require('base')
 require('highlights')
 require('maps')
 
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+-- By default, Vim doesn't sync yanked text with the system clipboard, and vice versa
+-- this is the sync code below
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+-- import macos.lua file
+  require('macos')
+end
+
+if is_win then
+  require('windows')
+end
+
